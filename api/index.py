1df38e75c1571ea8b248c5b1978bf557a4937008
Brian Liu <BLiu1@users.noreply.github.com>
2024-03-14 20:31:48 -0700

    Strip everything down to serverless function
    This still didn't work

--- api/index.py
+++ api/index.py
@@ -1,14 +1,9 @@
 from http.server import BaseHTTPRequestHandler
-# from main import main
 
 
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        # start up client, wait until command completion, return, continue
-        # TODO: move code from main to here
-        # main()
-        print('GET reached')
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
